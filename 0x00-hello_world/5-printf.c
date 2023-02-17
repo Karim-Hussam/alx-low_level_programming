291182bbca3cb774f7b5a664011c74f250bf8989
Karim Hussam Al-Din <xkarmhossamiv@gmail.com>
2023-02-17 12:41:43 -0800

    another commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - start of code
+ *Return: 0 means success
+*/
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
+
