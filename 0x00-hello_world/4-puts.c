96bd3556e92cec75804d9a4b86bbb3c11f734d32
Karim Hussam Al-Din <xkarmhossamiv@gmail.com>
2023-02-17 12:24:42 -0800

    another commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ *main - start of code
+ *return - 0 means success
+*/
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+		return(0);
 }
