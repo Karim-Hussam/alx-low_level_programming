871ec36344aeeceec2a1c37639a3d23b3cc1ce58
Karim Hussam Al-Din <xkarmhossamiv@gmail.com>
2023-02-17 12:50:19 -0800

    another commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - start of code
+ *Return: 1 means success
+*/
+
+int main(void)
+{
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
+}
+
